c8115acc5da34a1e5aa93752e8b349959243a058
José Pérez <jotangamers@gmail.com>
2025-09-10 13:59:02 -0400

    Adding some functions in add.js

--- js/add.js
+++ js/add.js
@@ -1,19 +1,42 @@
 /* variables */
 
-const carMade = document.getElementById("model").value;
-const carYear = document.getElementById("year").value;
-const carMillage = document.getElementById("millage").value;
-const sliceMenu = document.querySelectorAll(".slice-menu");
-const menuContent = sliceMenu.elements;
+const carMade = document.getElementById("model");
+const carYear = document.getElementById("year");
+const carMillage = document.getElementById("millage");
+const menuFeatures = document.querySelectorAll(".slice-menu input[type='checkbox']");
+const selectedFeatures = [];
+
 const note = document.getElementById("note");
-const noteContent = note.select();
+const sendBtn = document.querySelector(".send-btn");
 
 class Car {
-    constructor(make, year, millage, opFeatures) {
+    constructor(make, year, millage, opFeatures, note) {
         this.make = make;
         this.year = year;
         this.millage = millage;
         this.opFeatures = opFeatures;
+        this.note = note;
     }
 }
+
+const myCar = new Car(carMade.value, carYear.value, carMillage.value, menuFeatures.checked, note.value);
+
+sendBtn.addEventListener('click', () => {
+    menuFeatures.forEach(async (f) => {
+        if (f.checked) {
+            selectedFeatures.push(f.name);
+        }
+    });
+
+    localStorage.setItem("cars", JSON.stringify(Car));
+});
+
+console.log("Valor de make:", carMade.value);
+console.log("Valor de year:", carYear.value);
+console.log("Valor de millage:", carMillage.value);
+console.log("Checkboxes seleccionados:", selectedFeatures);
+console.log("Nota:", note.value);
+
+
+console.log("Guardado en localStorage:", JSON.parse(localStorage.getItem("cars")));
 
